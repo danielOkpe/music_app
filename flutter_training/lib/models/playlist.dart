96ce7f0d17b496ae1c39e48bdf2ea99eb4f709bb
unknown <daniel.okpe@epitech.eu>
2025-03-19 22:15:23 +0100

    add playlist

--- flutter_training/lib/models/playlist.dart
+++ flutter_training/lib/models/playlist.dart
@@ -2,8 +2,9 @@ import 'package:flutter_training/models/audio.dart';
 
 class Playlist{
   List<Audio> playlist;
+  String name;
 
-  Playlist({required this.playlist});
+  Playlist({required this.playlist, required this.name});
 
   add(Audio audio){
     playlist.add(audio);
