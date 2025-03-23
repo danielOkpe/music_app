b6ae6640e55ca1cd027f0c565e8086f75fa270d8
unknown <daniel.okpe@epitech.eu>
2025-03-23 23:06:55 +0100

    add animation

--- flutter_training/lib/views/widgets/all_widget.dart
+++ flutter_training/lib/views/widgets/all_widget.dart
@@ -3,43 +3,91 @@ import 'package:flutter_training/models/constants/constants.dart';
 import 'package:flutter_training/models/audio.dart';
 import 'package:flutter_training/views/widgets/cells/audio_cell.dart';
 
-class AllWidget extends StatelessWidget {
+class AllWidget extends StatefulWidget {
   const AllWidget({super.key});
 
   @override
+  State<AllWidget> createState() => _AllWidgetState();
+}
+
+class _AllWidgetState extends State<AllWidget> {
+
+  List<double> opacities = [];
+  List<double> translations = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
+
+  void _startAnimations() {
+    int totalItems = Constants.SONGLIST.length + Constants.PODCASTLIST.length + Constants.AUDIOBOOK.length;
+
+    opacities = List.generate(totalItems, (_) => 0.0);
+    translations = List.generate(totalItems, (_) => -20.0);
+
+    for (int i = 0; i < totalItems; i++) {
+      Future.delayed(Duration(milliseconds: i * 200), () {
+        if (mounted) {
+          setState(() {
+            opacities[i] = 1.0;
+            translations[i] = 0.0;
+          });
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
+    return  Column(
       children: [
         SizedBox(
           height: 20,
         ),
         SizedBox(
           height: 200,
-          child:_buildListView(Constants.SONGLIST),
+          child:_buildAnimatedListView(Constants.SONGLIST, 0),
         ),
         SizedBox(
           height: 20,
         ),
         SizedBox(
           height: 200,
-          child:_buildListView(Constants.PODCASTLIST),
+          child:_buildAnimatedListView(Constants.PODCASTLIST, Constants.SONGLIST.length),
         ),
         SizedBox(
           height: 20,
         ),
         SizedBox(
-          height: 200,
-          child: _buildListView(Constants.AUDIOBOOK)
+            height: 200,
+            child: _buildAnimatedListView(Constants.AUDIOBOOK, Constants.SONGLIST.length + Constants.PODCASTLIST.length )
         )
       ],
     );
   }
 
- Widget _buildListView(List<Audio> audios){
-    return ListView.builder(
+
+  Widget _buildAnimatedListView(List<Audio> audios, int startIndex) {
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
         itemCount: audios.length,
         scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext context, int index) => AudioCell(playlist: audios, index: index,)
+        itemBuilder: (BuildContext context, int index) {
+          int globalIndex = startIndex + index;
+
+          return AnimatedOpacity(
+            duration: Duration(milliseconds: 500),
+            opacity: opacities[globalIndex],
+            child: Transform.translate(
+              offset: Offset(0, translations[globalIndex]),
+              child: AudioCell(playlist: audios, index: index),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- flutter_training/lib/views/widgets/songs_widget.dart
+++ flutter_training/lib/views/widgets/songs_widget.dart
@@ -2,18 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_training/views/widgets/cells/audio_cell.dart';
 import 'package:flutter_training/models/constants/constants.dart';
 
-class SongsWidget extends StatelessWidget {
-   const SongsWidget({super.key});
+class SongsWidget extends StatefulWidget {
+  const SongsWidget({super.key});
 
   @override
+  State<SongsWidget> createState() => _SongsWidgetState();
+}
+
+class _SongsWidgetState extends State<SongsWidget> {
+  double _opacity = 0.0;
+
+
+  _startAnimation(){
+    Future.delayed(Duration(milliseconds: 100), () {
+      if (mounted) {
+        setState(() {
+          _opacity = 1.0;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _startAnimation();
+  }
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      child: ListView.builder(
+    return   AnimatedOpacity(
+      duration: Duration(seconds: 1),
+      opacity: _opacity,
+      child: SizedBox(
+        height: 200,
+        child: ListView.builder(
           itemCount: Constants.SONGLIST.length,
           scrollDirection: Axis.horizontal,
-          itemBuilder: (context,index) => AudioCell(playlist: Constants.SONGLIST, index: index)
+          itemBuilder: (context, index) => AudioCell(
+            playlist: Constants.SONGLIST,
+            index: index,
+          ),
+        ),
       ),
     );
   }
 }
+
